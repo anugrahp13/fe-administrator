b40960821d742eda7bc8558dc6acd2327182fa85
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-11 17:52:48 +0700

    feat[app-sidebar.tsx]: Create file app-sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { dataMenu } from "@/data/dataMenu";
+import { NavMain } from "@/components/nav-main";
+import { NavManagements } from "@/components/nav-managements";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={dataMenu.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={dataMenu.navMain} />
+        <NavManagements managements={dataMenu.managements} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={dataMenu.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
