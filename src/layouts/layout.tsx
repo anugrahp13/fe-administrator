35e8941bb88b19a9c8e608faa6051f5e369d349d
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-13 13:39:44 +0700

    feat[layout.tsx]: added properties themeprovider for theme dark mode

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,15 +1,18 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
 import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Header />
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
