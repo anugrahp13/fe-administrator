b363c886b9e66f558038d197d0223a53e523982a
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-13 13:37:18 +0700

    refactor[header.tsx]: added dropdown menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -7,24 +10,34 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Bell } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Link } from "react-router-dom";
+import { ModeToggle } from "./mode-toggle";
+
 interface HeaderProps {
   children?: React.ReactNode;
 }
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <>
-      <div className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div className="flex items-center gap-2 px-4">
+      <div className="flex justify-between px-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2" />
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
+                <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
               </BreadcrumbItem>
               <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem>
@@ -32,6 +45,70 @@ export const Header: React.FC<HeaderProps> = () => {
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
+        </div>
+        <div className="flex">
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Bell />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-80 mr-4">
+                <DropdownMenuLabel className="font-semibold">
+                  Notifications
+                </DropdownMenuLabel>
+                {/* <DropdownMenuSeparator /> */}
+                <DropdownMenuGroup className="space-y-2">
+                  <DropdownMenuItem>
+                    <div className="flex gap-2">
+                      <Avatar>
+                        <AvatarImage
+                          src="/image/avatar/avatar.png"
+                          alt="@shadcn"
+                        />
+                        <AvatarFallback>HM</AvatarFallback>
+                      </Avatar>
+                      <div className="flex flex-col gap-1">
+                        <p className="text-sm font-semibold">Anugrah Prastyo</p>
+                        <p className="text-sm">
+                          <span className="text-red-400">Edit</span> - Harga
+                          Gula Putih GMP 1Kg
+                        </p>
+                        <p className="text-xs text-gray-400">30 menit lalu</p>
+                      </div>
+                    </div>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <div className="flex gap-2">
+                      <Avatar>
+                        <AvatarImage
+                          src="https://github.com/shadcn.png"
+                          alt="@shadcn"
+                        />
+                        <AvatarFallback>HM</AvatarFallback>
+                      </Avatar>
+                      <div className="flex flex-col gap-1">
+                        <p className="text-sm font-semibold">Madkarni</p>
+                        <p className="text-sm">
+                          <span className="text-red-400">Delete</span> - Teh
+                          Sariwangi Kotak
+                        </p>
+                        <p className="text-xs text-gray-400">30 menit lalu</p>
+                      </div>
+                    </div>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to={""} className="text-center">
+                    Show more
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <ModeToggle />
         </div>
       </div>
     </>
