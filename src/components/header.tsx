138cd9d7dcc0b7cb6c59ff9d1ca84822a6c776d8
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-13 18:37:23 +0700

    refactor[header.tsx]: remove breadcrumblist and import selector navNotification

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,8 @@
 import React from "react";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Bell } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Link } from "react-router-dom";
+
 import { ModeToggle } from "./mode-toggle";
+import { NavNotification } from "./nav-notification";
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -33,81 +13,10 @@ export const Header: React.FC<HeaderProps> = () => {
       <div className="flex justify-between px-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <div className="flex items-center gap-2">
           <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+          {/* <Separator orientation="vertical" className="mr-2" /> */}
         </div>
         <div className="flex">
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                  <Bell />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-80 mr-4">
-                <DropdownMenuLabel className="font-semibold">
-                  Notifications
-                </DropdownMenuLabel>
-                {/* <DropdownMenuSeparator /> */}
-                <DropdownMenuGroup className="space-y-2">
-                  <DropdownMenuItem>
-                    <div className="flex gap-2">
-                      <Avatar>
-                        <AvatarImage
-                          src="/image/avatar/avatar.png"
-                          alt="@shadcn"
-                        />
-                        <AvatarFallback>HM</AvatarFallback>
-                      </Avatar>
-                      <div className="flex flex-col gap-1">
-                        <p className="text-sm font-semibold">Anugrah Prastyo</p>
-                        <p className="text-sm">
-                          <span className="text-red-400">Edit</span> - Harga
-                          Gula Putih GMP 1Kg
-                        </p>
-                        <p className="text-xs text-gray-400">30 menit lalu</p>
-                      </div>
-                    </div>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <div className="flex gap-2">
-                      <Avatar>
-                        <AvatarImage
-                          src="https://github.com/shadcn.png"
-                          alt="@shadcn"
-                        />
-                        <AvatarFallback>HM</AvatarFallback>
-                      </Avatar>
-                      <div className="flex flex-col gap-1">
-                        <p className="text-sm font-semibold">Madkarni</p>
-                        <p className="text-sm">
-                          <span className="text-red-400">Delete</span> - Teh
-                          Sariwangi Kotak
-                        </p>
-                        <p className="text-xs text-gray-400">30 menit lalu</p>
-                      </div>
-                    </div>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <Link to={""} className="text-center">
-                    Show more
-                  </Link>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
+          <NavNotification />
           <ModeToggle />
         </div>
       </div>
