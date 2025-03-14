f2bd896c08b51359eab82d57a1dfcfcc9b12bfc8
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-15 00:04:57 +0700

    feat[sales-card.tsx]: add feature component sales card

---
+++ src/pages/dashboard/sales-card.tsx
@@ -0,0 +1,66 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "../../components/ui/avatar";
+import { dataSales } from "@/data/dataSales";
+import { DatePickerRange } from "@/components/date-pickerrange";
+
+export function SalesCard() {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <div className="flex justify-between">
+          <CardTitle className="font-bold">Recent Sales</CardTitle>
+          <CardDescription className="dark:text-white font-semibold">
+            Top 10 Customers
+          </CardDescription>
+        </div>
+        <CardDescription className="flex justify-center items-center font-semibold">
+          <DatePickerRange />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        {dataSales.sales
+          .slice()
+          .sort((a, b) => b.value - a.value)
+          .map((salesPerson, index) => (
+            <div key={salesPerson.id} className="flex flex-col">
+              {/* Avatar & Nama */}
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  <div className="font-semibold">{index + 1}.</div>
+                  <Avatar>
+                    <AvatarImage
+                      src={salesPerson.avatar}
+                      alt={salesPerson.name}
+                    />
+                    <AvatarFallback>
+                      {salesPerson.name
+                        .split(" ")
+                        .map((n) => n[0])
+                        .join("")}
+                    </AvatarFallback>
+                  </Avatar>
+                  <p className="text-sm font-semibold">{salesPerson.name}</p>
+                </div>
+                <div className="font-bold">
+                  <p>Rp. {salesPerson.value.toLocaleString("id-ID")}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </CardContent>
+      {/* <CardFooter>
+        <DatePickerRange />
+      </CardFooter> */}
+    </Card>
+  );
+}
