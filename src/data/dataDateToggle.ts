adc1b3783a186148b601193112485182bece2c53
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-14 17:46:12 +0700

    feat[dataDateToggle.ts]: add feature typescript project for dataDateToggle

---
+++ src/data/dataDateToggle.ts
@@ -0,0 +1,20 @@
+const dataDateToggle = [
+  {
+    id: 1,
+    name: "Today",
+  },
+  {
+    id: 2,
+    name: "Week",
+  },
+  {
+    id: 3,
+    name: "7 days",
+  },
+  {
+    id: 4,
+    name: "30 days",
+  },
+];
+
+export default dataDateToggle;
