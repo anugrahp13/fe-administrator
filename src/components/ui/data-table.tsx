37e3ec8cb92eafcc06a1fe160ddff3d1e7bd801c
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-17 01:05:59 +0700

    refactor[data-table.tsx]: remove icon ChevronDown and add icon PackagePlus Trash, and add button for add, delete item

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -9,15 +9,16 @@ import {
 } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ChevronDown } from "lucide-react";
-import { useDataTableStore } from "@/store/useDataTableStore";
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { PackagePlus, Trash2 } from "lucide-react";
+import { useDataTableStore } from "@/store/useDataTableStore";
 
 interface DataTableProps<TData> {
   columns: ColumnDef<TData>[];
@@ -25,9 +26,21 @@ interface DataTableProps<TData> {
   filterPlaceholder?: string;
 }
 
-export function DataTable<TData>({ columns, data, filterPlaceholder = "Search..." }: DataTableProps<TData>) {
-  const { sorting, setSorting, columnFilters, setColumnFilters, columnVisibility, setColumnVisibility, rowSelection, setRowSelection } =
-    useDataTableStore();
+export function DataTable<TData>({
+  columns,
+  data,
+  filterPlaceholder = "Search...",
+}: DataTableProps<TData>) {
+  const {
+    sorting,
+    setSorting,
+    columnFilters,
+    setColumnFilters,
+    columnVisibility,
+    setColumnVisibility,
+    rowSelection,
+    setRowSelection,
+  } = useDataTableStore();
 
   const table = useReactTable({
     data,
@@ -45,32 +58,47 @@ export function DataTable<TData>({ columns, data, filterPlaceholder = "Search...
 
   return (
     <div className="w-full">
-      <div className="flex items-center py-2">
+      <div className="flex justify-between items-center py-2">
         <Input
           placeholder={filterPlaceholder}
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-          onChange={(e) => table.getColumn("email")?.setFilterValue(e.target.value)}
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(e) =>
+            table.getColumn("name")?.setFilterValue(e.target.value)
+          }
           className="max-w-sm"
         />
-        <DropdownMenu>
+        <div className="flex gap-1">
+          <Button variant="outline">
+            <PackagePlus />
+            Add Item
+          </Button>
+          <Button variant="destructive">
+            <Trash2 />
+            Delete All Item
+          </Button>
+        </div>
+        {/* <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
               Columns <ChevronDown />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            {table.getAllColumns().filter((column) => column.getCanHide()).map((column) => (
-              <DropdownMenuCheckboxItem
-                key={column.id}
-                checked={column.getIsVisible()}
-                onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                className="capitalize"
-              >
-                {column.id}
-              </DropdownMenuCheckboxItem>
-            ))}
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => (
+                <DropdownMenuCheckboxItem
+                  key={column.id}
+                  checked={column.getIsVisible()}
+                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                  className="capitalize"
+                >
+                  {column.id}
+                </DropdownMenuCheckboxItem>
+              ))}
           </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu> */}
       </div>
       <div className="rounded-md border">
         <Table>
@@ -79,7 +107,12 @@ export function DataTable<TData>({ columns, data, filterPlaceholder = "Search...
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
                   <TableHead key={header.id}>
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
                   </TableHead>
                 ))}
               </TableRow>
@@ -88,15 +121,26 @@ export function DataTable<TData>({ columns, data, filterPlaceholder = "Search...
           <TableBody>
             {table.getRowModel().rows.length ? (
               table.getRowModel().rows.map((row) => (
-                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
                   ))}
                 </TableRow>
               ))
             ) : (
               <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
                   No results.
                 </TableCell>
               </TableRow>
@@ -105,10 +149,20 @@ export function DataTable<TData>({ columns, data, filterPlaceholder = "Search...
         </Table>
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
-        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
           Previous
         </Button>
-        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
           Next
         </Button>
       </div>
