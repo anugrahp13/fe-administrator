be603ec17858b985db06f147090378a385fb4772
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-19 12:54:58 +0700

    refactor[item.tsx]: Added the alert dialog, and added code from the data-table file to the itempage for maintanance

--- src/pages/item/item.tsx
+++ src/pages/item/item.tsx
@@ -11,29 +11,53 @@ import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
 import { useState } from "react";
 import { useTableStore } from "@/store/tableStore";
 import dataItem, { DataItem } from "@/data/dataItem";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PackagePlus, Trash2 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 export default function ItemsPage() {
   const [filterStatus, setFilterStatus] = useState<
     "Ready" | "Sold Out" | "All" | "Limit Stock"
   >("All");
+  const [searchQuery, setSearchQuery] = useState("");
   const { columns } = useTableStore();
-  const filteredData: DataItem[] =
-    filterStatus === "All"
-      ? dataItem
-      : filterStatus === "Limit Stock"
-      ? dataItem.filter((item) => item.stock <= 5)
-      : dataItem.filter((item) => item.status === filterStatus);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const filteredData: DataItem[] = dataItem
+    .filter((item) => {
+      if (filterStatus !== "All" && filterStatus !== "Limit Stock") {
+        return item.status === filterStatus;
+      }
+      if (filterStatus === "Limit Stock") {
+        return item.stock <= 5;
+      }
+      return true;
+    })
+    .filter(
+      (item) =>
+        item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        item.category.toLowerCase().includes(searchQuery.toLowerCase())
+    );
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="flex justify-between w-full mx-auto gap-1">
-        <div className="flex flex-col">
+      <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center py-2 sm:gap-0 w-full mx-auto gap-1">
+        <div className="flex flex-col items-center">
           <div className="text-2xl lg:text-3xl font-bold">Item List</div>
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbItem>
                 <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
               </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbSeparator />
               <BreadcrumbItem>
                 <BreadcrumbPage>Item List</BreadcrumbPage>
               </BreadcrumbItem>
@@ -68,11 +92,44 @@ export default function ItemsPage() {
           </Menubar>
         </div>
       </div>
-      <DataTable
-        columns={columns}
-        data={filteredData}
-        filterPlaceholder="Search items . . ."
-      />
+      <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center py-2 gap-2 sm:gap-0">
+        <Input
+          placeholder="Search items . . ."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="max-w-sm"
+        />
+        <div className="flex gap-1">
+          <Button variant="outline">
+            <PackagePlus />
+            Add Item
+          </Button>
+          <Button variant="destructive" onClick={() => setIsModalOpen(true)}>
+            <Trash2 />
+            Delete Item
+          </Button>
+        </div>
+      </div>
+      <DataTable columns={columns} data={filteredData} />
+
+      {/* Modal Konfirmasi */}
+      <AlertDialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Ingin menghapus produk ini?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Apakah benar produk ini yang akan kamu hapus, coba renungkan
+              kembali.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="order-1">Cancel</AlertDialogCancel>
+            <AlertDialogAction className="bg-red-600 hover:bg-red-500 lg:order-2">
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
