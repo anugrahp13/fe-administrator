159cdb6870b61ac56e483bb7a2a2541787e6c09f
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-19 12:52:59 +0700

    refactor[data-table.tsx]: change the data-table code so that it can be reused on another page

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -7,8 +7,6 @@ import {
   getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -17,89 +15,26 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { PackagePlus, Trash2 } from "lucide-react";
-import { useDataTableStore } from "@/store/useDataTableStore";
+import { Button } from "./button";
 
 interface DataTableProps<TData> {
   columns: ColumnDef<TData>[];
   data: TData[];
-  filterPlaceholder?: string;
 }
 
-export function DataTable<TData>({
-  columns,
-  data,
-  filterPlaceholder = "Search...",
-}: DataTableProps<TData>) {
-  const {
-    sorting,
-    setSorting,
-    columnFilters,
-    setColumnFilters,
-    columnVisibility,
-    setColumnVisibility,
-    rowSelection,
-    setRowSelection,
-  } = useDataTableStore();
-
+export function DataTable<TData>({ columns, data }: DataTableProps<TData>) {
   const table = useReactTable({
     data,
     columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: { sorting, columnFilters, columnVisibility, rowSelection },
   });
 
   return (
     <div className="w-full">
-      <div className="flex justify-between items-center py-2">
-        <Input
-          placeholder={filterPlaceholder}
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(e) =>
-            table.getColumn("name")?.setFilterValue(e.target.value)
-          }
-          className="max-w-sm"
-        />
-        <div className="flex gap-1">
-          <Button variant="outline">
-            <PackagePlus />
-            Add Item
-          </Button>
-          <Button variant="destructive">
-            <Trash2 />
-            Delete All Item
-          </Button>
-        </div>
-        {/* <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns <ChevronDown />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => (
-                <DropdownMenuCheckboxItem
-                  key={column.id}
-                  checked={column.getIsVisible()}
-                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                  className="capitalize"
-                >
-                  {column.id}
-                </DropdownMenuCheckboxItem>
-              ))}
-          </DropdownMenuContent>
-        </DropdownMenu> */}
-      </div>
+      {/* Table Content */}
       <div className="rounded-md border">
         <Table>
           <TableHeader>
@@ -121,26 +56,17 @@ export function DataTable<TData>({
           <TableBody>
             {table.getRowModel().rows.length ? (
               table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
+                <TableRow key={row.id}>
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
                   ))}
                 </TableRow>
               ))
             ) : (
               <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
+                <TableCell colSpan={columns.length} className="h-24 text-center">
                   No results.
                 </TableCell>
               </TableRow>
@@ -148,6 +74,7 @@ export function DataTable<TData>({
           </TableBody>
         </Table>
       </div>
+      {/* Pagination */}
       <div className="flex items-center justify-end space-x-2 py-4">
         <Button
           variant="outline"
