d59b22ca6475251c59800c9138de7aae34b57b5d
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-22 18:03:40 +0700

    refactor[dataItem.tsx]: change name sellingPrice and purchasePrice to sellPrice and buyPrice. And add image

--- src/data/dataItem.ts
+++ src/data/dataItem.ts
@@ -1,57 +1,63 @@
 export type DataItem = {
-    id: string;
-    name: string;
-    category: string;
-    stock: number;
-    sellingPrice: number;
-    purchasePrice: number;
-    status: "Ready" | "Sold Out";
-  };
+  id: string;
+  image: string;
+  name: string;
+  category: string;
+  stock: number;
+  sellPrice: number;
+  buyPrice: number;
+  status: "Ready" | "Sold Out";
+};
 
 const dataItem: DataItem[] = [
   {
     id: "m5gr84i9",
+    image: "/image/avatar/avatar.png",
     name: "Gula Putih 1 kg",
     category: "Gula Pasir",
     stock: 290,
-    sellingPrice: 18500,
-    purchasePrice: 16400,
+    sellPrice: 18500,
+    buyPrice: 16400,
     status: "Ready",
   },
   {
     id: "3u1reuv4",
+    image: "/image/avatar/avatar.png",
     name: "Tepung Segitiga 1 kg",
     category: "Tepung",
     stock: 200,
-    sellingPrice: 10500,
-    purchasePrice: 9800,
+    sellPrice: 10500,
+    buyPrice: 9800,
     status: "Ready",
   },
   {
     id: "derv1ws0",
+    image: "/image/avatar/avatar.png",
     name: "Tepung Lencana 1 kg",
     category: "Tepung",
     stock: 3,
-    sellingPrice: 9000,
-    purchasePrice: 7650,
+    sellPrice: 9000,
+    buyPrice: 7650,
     status: "Sold Out",
   },
   {
     id: "5kma53ae",
+    image: "/image/avatar/avatar.png",
     name: "Minyak Goreng 1 kg",
     category: "Minyak Goreng",
     stock: 5,
-    sellingPrice: 21000,
-    purchasePrice: 18675,
+    sellPrice: 21000,
+    buyPrice: 18675,
     status: "Ready",
   },
   {
     id: "bhqecj4p",
+    image: "/image/avatar/avatar.png",
     name: "Tepung Besar 500 gr",
     category: "Tepung",
     stock: 4,
-    sellingPrice: 8000,
-    purchasePrice: 6789,
+    sellPrice: 8000,
+    buyPrice: 6789,
     status: "Sold Out",
   },
 ];
